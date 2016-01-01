74e8767c057b03fd2c186d7abf2ac1196d403d5d
MrLokans <trikster1911@gmail.com>
2016-01-01 17:32:13 +0300

    Added blog entry model.

---
+++ quick_blog/blog/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('title', models.CharField(max_length=180)),
+                ('body', models.TextField()),
+                ('slug', models.SlugField(max_length=180, unique=True)),
+                ('published', models.BooleanField(default=False)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Blog Posts',
+                'verbose_name': 'Blog Post',
+                'ordering': ['-created'],
+            },
+        ),
+    ]
--- quick_blog/blog/models.py
+++ quick_blog/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class EntryQuerySet(models.QuerySet):
+
+    def published(self):
+        """Return published blog entries"""
+        return self.filter(published=True)
+
+
+class Entry(models.Model):
+
+    class Meta:
+        verbose_name = 'Blog Post'
+        verbose_name_plural = 'Blog Posts'
+        ordering = ["-created"]
+
+    title = models.CharField(max_length=180)
+    body = models.TextField()
+    # field to store human-readable urls
+    slug = models.SlugField(max_length=180, unique=True)
+    published = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    # we extand filtering methods this way
+    objects = EntryQuerySet.as_manager()
+
+    def __str__(self):
+        return self.title
+
